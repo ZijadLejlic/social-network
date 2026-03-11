4acde39f8c093740732ba5e47ea360174edee6f6
yozi97 <lejliczijad@gmail.com>
2026-03-11 13:53:28 +0100

    Add Validation

--- js/app.js
+++ js/app.js
@@ -5,3 +5,41 @@ document.querySelector('#registracija').addEventListener('click', () => {
 document.querySelector('#close-modal').addEventListener('click', () => {
     document.querySelector('.custom-modal').style.display = 'none';
 });
+
+let config = {
+    'korisnicko_ime': {
+        required: true,
+        minlength: 5,
+        maxlength: 15
+    },
+    'register_email': {
+        required: true,
+        email: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    'register_lozinka': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'ponovi_lozinku'
+    },
+    'ponovi_lozinku': {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'register_lozinka'
+    }
+};
+
+let validator = new Validator(config, '#registrationForm');
+
+document.querySelector('#registrationForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(validator.validationPassed()) {
+        alert('Formular je validan');
+    } else {
+        alert('Formular nije validan');
+    }
+});
